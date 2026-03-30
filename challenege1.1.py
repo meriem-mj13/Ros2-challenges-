279cef438fd888e20bacc3b8ea1a1e171a840fed
meriem-mj13 <meriemmedjber2@gmail.com>
2026-03-30 13:54:09 +0100

    Update challenege1.1.py

--- challenege1.1.py
+++ challenege1.1.py
@@ -1,26 +1,22 @@
+# Écrire un node qui logue 'Hello ROS2!' toutes les 2 secondes.
+
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Float64
-import random
 
 
-class TemperaturePublisher(Node):
+class HelloROS2Node(Node):
 
     def __init__(self):
-        super().__init__('temperature_publisher')
-        self.publisher_ = self.create_publisher(Float64, '/temperature', 10)
-        self.create_timer(1.0, self.timer_callback)
+        super().__init__('hello_ros2_node')
+        self.create_timer(2.0, self.timer_callback)
 
     def timer_callback(self):
-        msg = Float64()
-        msg.data = 20.0 + random.uniform(-1.0, 1.0)
-        self.publisher_.publish(msg)
-        self.get_logger().info(f'Publishing temperature: {msg.data:.2f} °C')
+        self.get_logger().info('Hello ROS2!')
 
 
 def main(args=None):
     rclpy.init(args=args)
-    node = TemperaturePublisher()
+    node = HelloROS2Node()
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
