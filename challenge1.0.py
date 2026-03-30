f9cba57835061634d702e7f4f386db0ebb5871c8
meriem-mj13 <meriemmedjber2@gmail.com>
2026-03-30 13:24:55 +0100

    Add files via upload

---
+++ challenge1.0.py
@@ -0,0 +1,24 @@
+import rclpy
+from rclpy.node import Node
+
+
+class HelloROS2Node(Node):
+
+    def __init__(self):
+        super().__init__('hello_ros2_node')
+        self.create_timer(2.0, self.timer_callback)
+
+    def timer_callback(self):
+        self.get_logger().info('Hello ROS2!')
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = HelloROS2Node()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
